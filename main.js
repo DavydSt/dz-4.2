785f6b57fa0e414e80453cdc9329976932106c4e
DavydSt <145857522+DavydSt@users.noreply.github.com>
2025-09-07 19:00:34 +0200

    finish

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 const photo = document.querySelector(".slider__image");
+const slider = document.querySelector(".slider__input");
+
+const sliderChange = (value) => {
+  const scale = value / 100;
+  photo.style.transform = `scale(${scale})`;
+};
+
+const debouncedChange = _.debounce(sliderChange, 100);
+
+slider.addEventListener("input", () => {
+  debouncedChange(slider.value);
+});
